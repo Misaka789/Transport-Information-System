69fcdcc9308abdac149a2e5fc1f1231352049922
Misaka789 <3192422357@qq.com>
2024-07-05 22:52:20 +0800

    增加main函数的主干

--- func.h
+++ func.h
@@ -1,8 +1,37 @@
 #pragma once
 #include<iostream>
 using namespace std;
-void menu1()
-{
-	cout << "\t" << "欢迎使用交通咨询系统" << endl;
+//void MainMenu()
+//{
+//	cout << "\t" << "欢迎使用交通咨询系统" << endl;
+//	cout << "\t" << "1.输入城市及相关路线" << endl;
+//	cout << "\t" << "2.修改相关路线" << endl;
+//	cout << "\t" << "3.删除相关路线" << endl;
+//	cout << "\t" << "4.增加相关路线" << endl;
+//	cout << "\t" << "5.对两地做出最优决策(最短时间 & 最小开销)" << endl;
+//	cout << "\t" << "0.退出程序" << endl;
+//}
 
-}
+
+void ModefyLine() 
+{
+
+
+
+};
+void DeleteLine() 
+{
+
+
+
+};
+void BestLine()
+{
+
+
+};
+void AddLine()
+{
+
+
+};
--- main.cpp
+++ main.cpp
@@ -7,8 +7,30 @@ using namespace std;
 
 int main()
 {
-	menu1();
-
-
+	while (true) {
+		MainMenu();
+        int choice;
+        cin >> choice;
+        switch (choice)
+        {
+        case 0:
+            return false;   //输入0退出循环
+        case 1:
+            AddLine();   //增加线路函数
+            break;
+        case 2:
+            ModefyLine();    //修改线路函数
+            break;
+        case 3:
+            DeleteLine();    //删除线路函数
+            break;
+        case 4:
+            BestLine();      ///决策函数
+            break;
+        default:
+            cout << "无效选择，请重试。" << endl;
+            break;
+        }
+	}
 	return 0;
 }
